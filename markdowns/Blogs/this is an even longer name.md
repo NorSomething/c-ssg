0346be31132a4e7a1dc921f8c65d9c3e11dee58f
NorSomething <mcnirmalkumar00@gmail.com>
2026-07-19 19:35:13 +0530

    main file into funcs + subfolders as items in navbar (md files in subfolders as list)

---
+++ markdowns/Blogs/this is an even longer name.md
@@ -0,0 +1,5 @@
+## Hi everyone
+
+this is a paragraph
+this is **my bolded** name
+*italics*
--- src/main.c
+++ src/main.c
@@ -38,11 +38,68 @@
 "</body>\n" \
 "</html>"
 
+//global navbar on every page
+char navbar_html[8192] = "<nav class=\"navbar\">\n";
+
+void remove_md_extension(char* out, char* in, size_t out_size) {
+
+    strncpy(out, in, out_size-1);
+    out[out_size-1] = '\0';
+
+    char* dot = strchr(out, '.');
+    if (dot != NULL && strcmp(dot, ".md") == 0) {
+        *dot = '\0';
+    }
+
+}
+
+void make_html_page(char* output, char* body) {
+
+    char temp[256];
+    snprintf(temp, sizeof(temp), "../htmlfiles/%s.html", output);
+    FILE *fp = fopen(temp, "w");
+    if (fp == NULL) {
+        perror("Error when makign html file..");
+        exit(1);
+    }
+
+    fputs(HTML_BOILERPLATE, fp);
+    fputs(navbar_html, fp);
+    fputs(body, fp);
+    fputs(HTML_BOILERPLATE_ENDING, fp);
+    fclose(fp);
+
+}
+
+void parse_markdown_to_html(char* path, char* output) {
+
+    struct each_file file = get_struct(path);
+
+    if (file.lines == NULL) {
+        printf("Read error in %s, skipping...\n", path);
+        return;
+    }
+
+    char *parsed_line = parse_line(file.lines, file.line_count);
+
+    make_html_page(output, parsed_line);
+
+    //cleanup
+    free(parsed_line);
+    for (int j = 0; j < file.line_count; j++) {
+        free(file.lines[j]);
+    }
+    free(file.lines);
+
+}
 
 int main() {
 
     char list_of_markdown_names[127][128];
     int markdown_names_index = 0;
+
+    char list_of_folder_names[32][64];
+    int folder_name_index = 0;
 
     struct dirent *de; //dirent entry
     DIR *dir_stream = opendir("../markdowns");
@@ -54,6 +111,18 @@ int main() {
     while(((de = readdir(dir_stream)) != NULL)) {
 
         if ((strcmp(de->d_name, ".") == 0) || (strcmp(de->d_name, "..") == 0)) {
+            continue;
+        }
+
+        //if subfolder remember it
+        char full_path[512];
+        snprintf(full_path, sizeof(full_path), "../markdowns/%s", de->d_name);
+
+        struct stat st;
+        if (stat(full_path, &st) == 0 && S_ISDIR(st.st_mode)) {
+            strncpy(list_of_folder_names[folder_name_index], de->d_name, 64-1);
+            list_of_markdown_names[folder_name_index][64-1] = '\0';
+            folder_name_index++;
             continue;
         }
 
@@ -73,18 +142,10 @@ int main() {
     closedir(dir_stream);
 
     //building the nav bar
-    char navbar_html[8192] = "<nav class=\"navbar\">\n";
-
     for (int i = 0; i < markdown_names_index; i++) {
 
         char link_name[128];
-        strncpy(link_name, list_of_markdown_names[i], sizeof(link_name)-1);
-        link_name[sizeof(link_name)-1] = '\0';
-
-        //removing .md for the href display names thingy
-        char* dot = strrchr(link_name, '.');
-        if (dot != NULL && strcmp(dot, ".md") == 0)
-            *dot = '\0';
+        remove_md_extension(link_name, list_of_markdown_names[i], sizeof(link_name));
 
         char link_buffer[256];
         snprintf(link_buffer, sizeof(link_buffer), "    <a href=\"%s.html\">%s</a>\n", list_of_markdown_names[i], link_name);
@@ -92,65 +153,78 @@ int main() {
 
     }
 
+        //one navbar entry per subfolder
+        for (int i = 0; i < folder_name_index; i++) {
+
+            char link_buffer[256];
+            snprintf(link_buffer, sizeof(link_buffer), "    <a href=\"%s.html\">%s</a>\n", list_of_folder_names[i], list_of_folder_names[i]);
+            strcat(navbar_html, link_buffer);
+
+        }
+
+
     //all the stuff of the .md will be in this div
     strcat(navbar_html, "</nav>\n<div class=\"content-container\">\n");
 
     printf("Found %d markdown files. \n", markdown_names_index);
 
+    //rendering non directory md files
     for (int i = 0; i < markdown_names_index; i++) {
+        parse_markdown_to_html(list_of_markdown_names[i], list_of_markdown_names[i]);
+    }
 
-        struct each_file file_data = get_struct(list_of_markdown_names[i]);
+    //rendering md files inside a directory
+    for (int i = 0; i < folder_name_index; i++) {
 
-        if (file_data.lines == NULL) {
-            printf("Skipping %s : read error.\n", file_data.name);
+        char folder_path[256];
+        snprintf(folder_path, sizeof(folder_path), "../markdowns/%s", list_of_folder_names[i]);
+
+        DIR* folder_stream = opendir(folder_path);
+        if (folder_stream == NULL) {
+            printf("Error when opening folder : %s... skippinh\n", list_of_folder_names[i]);
             continue;
         }
 
-        printf("Contents of %s (%d lines) : \n", file_data.name, file_data.line_count);
+        char file_list_html[4096] = "<ol>\n";
+        struct dirent *fde; //file directory entry
 
-        for (int j = 0; j < file_data.line_count; j++) {
-            printf("%s\n", file_data.lines[j]);
-        }
+        while((fde = readdir(folder_stream)) != NULL) {
 
-        char *parsed_line = parse_line(file_data.lines, file_data.line_count);
-
-        printf("Parsed line of %s : \n", file_data.name);
-
-        printf("%s\n", parsed_line);
-
-        char temp[256];
-        snprintf(temp, sizeof(temp), "../htmlfiles/%s.html", list_of_markdown_names[i]);
-        FILE* fp = fopen(temp, "w");
-        if (fp == NULL) {
-            perror("Error in creating html file..");
-            exit(1);
-        }
-
-        char* html_boilerplate = HTML_BOILERPLATE;
-        char* html_boilerplate_ending = HTML_BOILERPLATE_ENDING;
-
-        fputs(html_boilerplate, fp);
-        fputs(navbar_html, fp);
-        fputs(parsed_line, fp);
-        fputs(html_boilerplate_ending, fp);
-
-        //cleanup
-        fclose(fp);
-
-        if (parsed_line != NULL) {
-            free(parsed_line);
-        }
-
-        if (file_data.lines != NULL) {
-            for (int j = 0; j < file_data.line_count; j++) {
-                free(file_data.lines[j]);
+            if ((strcmp(fde->d_name, ".") == 0) ||  (strcmp(fde->d_name, "..") == 0)) {
+                continue;
             }
-            free(file_data.lines); //freeing individual lines, then the whole array itself
+
+            size_t len = strlen(fde->d_name);
+
+            //same getting md file logic from dir as above
+            if (len < 3 || (strcmp(fde->d_name + len - 2, "md") != 0)) {
+                continue;
+            }
+
+            char link_name[128];
+            remove_md_extension(link_name, fde->d_name, sizeof(link_name));
+
+            char name_of_html[256];
+            snprintf(name_of_html, sizeof(name_of_html), "%s__%s", list_of_folder_names[i], link_name);
+
+            char dir_list_buffer[512];
+            snprintf(dir_list_buffer, sizeof(dir_list_buffer), "<li><a href=\"%s.html\">%s</a></li>\n", name_of_html, link_name);
+
+            char sub_path[512];
+            snprintf(sub_path, sizeof(sub_path), "%s/%s", list_of_folder_names[i], fde->d_name);
+            strcat(file_list_html, dir_list_buffer);
+
+            parse_markdown_to_html(sub_path, name_of_html);
+
         }
 
+        closedir(folder_stream);
+        strcat(file_list_html, "</ol>\n");
 
+        make_html_page(list_of_folder_names[i], file_list_html);
 
     }
 
     return 0;
+
 }
