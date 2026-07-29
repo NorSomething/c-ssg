e1692d192503bd96351f77363a4e1646384f5f64
NorSomething <mcnirmalkumar00@gmail.com>
2026-07-30 04:18:53 +0530

    forgot semicolon lol

--- src/main.c
+++ src/main.c
@@ -179,7 +179,7 @@ int main() {
     //rendering non directory md files
     for (int i = 0; i < markdown_names_index; i++) {
         char output_name[128];
-        remove_md_extension(output_name, list_of_markdown_names[i], sizeof(output_name))
+        remove_md_extension(output_name, list_of_markdown_names[i], sizeof(output_name));
         parse_markdown_to_html(list_of_markdown_names[i], list_of_markdown_names[i]);
     }
 
